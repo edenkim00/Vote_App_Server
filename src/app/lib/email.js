8ecb97140dd3c6904b6c040a56d1263294fe7a5d
edenkim00 <eotjd0986@kaist.ac.kr>
2025-02-09 20:45:07 +0900

    feat: multi email sender

--- src/app/lib/email.js
+++ src/app/lib/email.js
@@ -8,9 +8,53 @@ const path = require("path");
 
 const { generateCsv } = require("../utils/report");
 require("dotenv").config();
+async function sendEmailWithCommonSender(data) {
+  const { email, appName } = data;
+
+  let authNum = Math.random().toString().substring(2, 8);
+  console.log(authNum);
+  // 7001
+  if (email == null || email == "") return response(baseResponse.WRONG_BODY);
+  const mailPoster = nodemailer.createTransport(
+    smtpTransport({
+      service: "gmail",
+      auth: {
+        user: "bettertogetherprjt@gmail.com",
+        // eslint-disable-next-line no-undef
+        pass: process.env.COMMON_EMAIL_AUTH_PASSWORD,
+      },
+    })
+  );
+
+  const mailOptions = {
+    from: "bettertogetherprjt@gmail.com",
+    to: email,
+    subject: `[${
+      appName ?? "NLCS JEJU Better Together"
+    }] 이메일 인증번호를 안내해드립니다.`,
+    text: "인증번호는 " + authNum + " 입니다.",
+  };
+  return new Promise((resolve) => {
+    mailPoster.sendMail(mailOptions, function (error) {
+      if (error) {
+        //7002
+        console.error("[Send Email]: ", error);
+        return resolve(errResponse(baseResponse.EMAIL_SEND_ERROR));
+      } else {
+        resolve(response(baseResponse.SUCCESS, { code: authNum }));
+      }
+    });
+  });
+}
 
 exports.sendEmail = async function (data) {
-  const { email, shouldExist } = data;
+  const { email, shouldExist, appName } = data;
+
+  // MARK: - If appName is provided, use common email sender
+  if (appName) {
+    return await sendEmailWithCommonSender(data);
+  }
+
   if (shouldExist) {
     const doubleCheck = await Provider.getUserEmail(email);
     if (doubleCheck.length == 0) {
